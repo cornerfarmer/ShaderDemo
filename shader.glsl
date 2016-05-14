2ed5706c5a3a2f367258edb2b33a649402ad6e2d
domin1101 <dominik.winkelbauer@outlook.de>
2016-05-14 13:29:39 +0200

    [FEATURE] Makes specular and diffuse lightning working

--- shader.glsl
+++ shader.glsl
@@ -9,11 +9,12 @@ in vec4 gxl3d_TexCoord0;
 
 uniform mat4 gxl3d_ModelViewProjectionMatrix;
 uniform mat4 gxl3d_ModelViewMatrix;
+uniform mat4 gxl3d_ModelMatrix;
 out vec4 normal;
 out vec4 TexCoord;
 void main()
 {
-    normal = gxl3d_Normal;
+    normal = gxl3d_ModelMatrix * gxl3d_Normal;
    
     gl_Position = gxl3d_ModelViewProjectionMatrix * gxl3d_Position;
 	TexCoord = gxl3d_TexCoord0;
@@ -36,12 +37,12 @@ void main()
 	vec4 n = normalize(normal);
 
 	vec4 eye = normalize(-camera);
-    vec4 normLightSource = normalize(lightSource);
+    vec4 normLightSource = -1 * normalize(lightSource);
 
-    float intensity = max(dot(n, normLightSource), 0.0);
+    float intensity = max(dot(n, normLightSource), 0.15);
     vec4 H = normalize(eye + normLightSource);
-	float specular = max(pow(dot(H, n), 30), 0);
+	float specular = max(pow(dot(H, n), 100), 0);
 
-	Out_Color = texture(tex0, TexCoord.xy);
+	Out_Color = texture(tex0, TexCoord.xy) * intensity +specular;
 }
 
