7317b15cdb376a6ee63c48e1c73ae3b19f60a09c
domin1101 <dominik.winkelbauer@outlook.de>
2016-05-22 11:40:38 +0200

    [TASK] Reformats code and adds comments to all lua scripts

--- init.lua
+++ init.lua
@@ -1,9 +1,121 @@
 
+-- Load libs
 lib_dir = gh_utils.get_scripting_libs_dir()
 dofile(lib_dir .. "lua/gx_cam_lib_v1.lua")
 
 
+-- +++++++++++++++ Load models +++++++++++++++
+-- Build absolute paths
+local demo_dir = gh_utils.get_scenegraph_dir()
+local model_directory = demo_dir .. "Assets/"
+
+-- Load the lamppost
+model = gh_model.create_from_file_loader_obj("Lamppost.obj", model_directory, model_directory)
+if (model > 0) then
+	-- Textures
+	gh_model.load_textures(model, model_directory)
+	model_norm_tex = gh_texture.create_from_file_v2(model_directory .. "Metal_Normal.jpg", PF_U8_RGB, 1, 1)
+	-- Translate, rotate and scale
+	gh_object.set_position(model, 2.5, 0.2, 0)
+	gh_object.set_euler_angles(model, 0, 0, 0)
+	gh_object.set_scale(model, 1, 1, 1)
+end  
+
+-- Load the street
+street = gh_model.create_from_file_loader_obj("road.obj", model_directory, model_directory)
+if (street > 0) then
+	-- Textures
+	gh_model.load_textures(street, model_directory)
+	street_norm_tex = gh_texture.create_from_file_v2(model_directory .. "Roads0069_1_S_Normal.jpg", PF_U8_RGB, 1, 1)
+	-- Translate, rotate and scale
+	gh_object.set_position(street, -2, 0, 0)
+	gh_object.set_euler_angles(street, 0, 0, 0)
+	gh_object.set_scale(street, 1, 1, 1)
+	-- Recompute normals for better lightning
+	gh_object.compute_faces_normal(street)
+	gh_object.compute_vertices_normal(street)
+end  
+
+-- Load the sidewalk
+sidewalk = gh_model.create_from_file_loader_obj("sidewalk.obj", model_directory, model_directory)
+if (sidewalk > 0) then
+	-- Textures
+	gh_model.load_textures(sidewalk, model_directory)
+	sidewalk_norm_tex = gh_texture.create_from_file_v2(model_directory .. "FloorStreets_Normal.jpg", PF_U8_RGB, 1, 1)
+	-- Translate, rotate and scale
+	gh_object.set_position(sidewalk, 3.75, 0.15, 0)
+	gh_object.set_euler_angles(sidewalk, 0, 0, 0)
+	gh_object.set_scale(sidewalk, 1, 1, 1)
+	-- Recompute normals for better lightning
+	gh_object.compute_faces_normal(sidewalk)
+	gh_object.compute_vertices_normal(sidewalk)
+end  
+
+-- Load the wall
+wall = gh_model.create_from_file_loader_obj("wall.obj", model_directory, model_directory)
+if (wall > 0) then
+	-- Textures
+	gh_model.load_textures(wall, model_directory)
+	wall_norm_tex = gh_texture.create_from_file_v2(model_directory .. "BrickSmallBrown0257_1_S_Normal.jpg", PF_U8_RGB, 1, 1)
+	-- Translate, rotate and scale
+	gh_object.set_position(wall, 5.8, 2.09, 0)
+	gh_object.set_euler_angles(wall, 0, 0, 0)
+	gh_object.set_scale(wall, 1, 1, 1)
+	-- Recompute normals for better lightning
+	gh_object.compute_faces_normal(wall)
+	gh_object.compute_vertices_normal(wall)
+end  
+
+-- Load the wooden box
+box = gh_model.create_from_file_loader_obj("WoodenBoxOpen02.obj", model_directory, model_directory)
+if (box > 0) then
+	-- Textures
+	gh_model.load_textures(box, model_directory)
+	box_norm_tex = gh_texture.create_from_file_v2(model_directory .. "WoodPlanks_Normal.jpg", PF_U8_RGB, 1, 1)
+	-- Translate, rotate and scale
+	gh_object.set_position(box, 4.5, 1.5, -5)
+	gh_object.set_euler_angles(box, 180, 0, 0)
+	gh_object.set_scale(box, 2.3, 2.3, 2.3)
+end  
+
+-- Load the aircondition
+aircondition = gh_model.create_from_file_loader_obj("AIRCONDITION.obj", model_directory, model_directory)
+if (aircondition > 0) then
+	-- Textures
+	gh_model.load_textures(aircondition, model_directory)
+	aircondition_norm_tex = gh_texture.create_from_file_v2(model_directory .. "AIRCONDITIONED_Normal.jpg", PF_U8_RGB, 1, 1)
+	-- Translate, rotate and scale
+	gh_object.set_position(aircondition, 5.5, 6, 4)
+	gh_object.set_euler_angles(aircondition, 0, 0, 0)
+	gh_object.set_scale(aircondition, 1, 1, 1)
+end  
+-- Fullscreen quad
+fullscreen_quad = gh_mesh.create_quad(800, 600)
+  
+-- +++++++++++++++ Load shaders +++++++++++++++
+-- Main scene
+shader_prog = gh_gpu_program.create_from_file("shader.glsl", 0)
+gh_node.set_name(shader_prog, "shader_prog")
+-- Shadow map
+shader_shadowMap = gh_gpu_program.create_from_file("shader_shadowMap.glsl", 0)
+gh_node.set_name(shader_shadowMap, "shader_shadowMap")
+-- Shadow map viewer
+shadowmap_viewer_prog = gh_gpu_program.create_from_file("shadowmap_viewer.glsl", 0)
+gh_node.set_name(shadowmap_viewer_prog, "shadowmap_viewer_prog")
+
+-- +++++++++++++++ Create render target +++++++++++++++
+shadowMap_w = 4096
+shadowMap_h = 4096
+shadowMap = gh_render_target.create(shadowMap_w, shadowMap_h)
+
+-- +++++++++++++++ Init lights +++++++++++++++
+lightDegree = math.pi / 2 * 3
+lightSource = { - 10, 10, 0 }
+lightLookAt = { 0, 0, 0 }
+
+-- +++++++++++++++ Init cameras +++++++++++++++
 winW, winH = gh_window.getsize(0)
+-- Main camera
 camera = gx_camera.create_perspective(60, 1, 0, 0, winW, winH, 0.1, 1000)
 gh_camera.set_position(camera, 5, 5, 5)
 camera_lookat_x = 0
@@ -11,126 +123,16 @@ camera_lookat_y = 5
 camera_lookat_z = 3
 gx_camera.init_orientation(camera, camera_lookat_x, camera_lookat_y, camera_lookat_z, 20, 170)
 gx_camera.set_mode_orbit()
-orbit_mode = 1
-keyboard_speed = 10.0
-camera_fov = 60.0
 gx_camera.set_orbit_lookat(camera, camera_lookat_x, camera_lookat_y, camera_lookat_z)
-gx_camera.set_keyboard_speed(keyboard_speed)
-gh_model.set_current_3d_obj_loader("ObjLoaderV2")
-cube_size = 1
-local demo_dir = gh_utils.get_scenegraph_dir()
-local model_directory = demo_dir .. "Assets/"
-model = gh_model.create_from_file_loader_obj("Lamppost.obj", model_directory, model_directory)
-street = gh_model.create_from_file_loader_obj("road.obj", model_directory, model_directory)
-sidewalk = gh_model.create_from_file_loader_obj("sidewalk.obj", model_directory, model_directory)
-wall = gh_model.create_from_file_loader_obj("wall.obj", model_directory, model_directory)
-box = gh_model.create_from_file_loader_obj("WoodenBoxOpen02.obj", model_directory, model_directory)
-aircondition = gh_model.create_from_file_loader_obj("AIRCONDITION.obj", model_directory, model_directory)
-if (model > 0) then
-	gh_model.load_textures(model, model_directory)
-
-	model_norm_tex = gh_texture.create_from_file_v2(model_directory .. "Metal_Normal.jpg", PF_U8_RGB, 1, 1)
-
-	gh_object.set_materials_texture_unit_offset(model, 0)
-
-	gh_object.set_position(model, 2.5, 0.2, 0)
-	gh_object.set_euler_angles(model, 0, 0, 0)
-	gh_object.set_scale(model, 1, 1, 1)
-
-end  
-if (street > 0) then
-	gh_model.load_textures(street, model_directory)
-
-	street_norm_tex = gh_texture.create_from_file_v2(model_directory .. "Roads0069_1_S_Normal.jpg", PF_U8_RGB, 1, 1)
-
-	gh_object.set_materials_texture_unit_offset(street, 0)
-
-	gh_object.set_position(street, -2, 0, 0)
-	gh_object.set_euler_angles(street, 0, 0, 0)
-	gh_object.set_scale(street, 1, 1, 1)
-
-	gh_object.compute_faces_normal(street)
-	gh_object.compute_vertices_normal(street)
-end  
-if (sidewalk > 0) then
-	gh_model.load_textures(sidewalk, model_directory)
-	gh_object.set_materials_texture_unit_offset(sidewalk, 0)
-
-	sidewalk_norm_tex = gh_texture.create_from_file_v2(model_directory .. "FloorStreets_Normal.jpg", PF_U8_RGB, 1, 1)
-
-	gh_object.set_position(sidewalk, 3.75, 0.15, 0)
-	gh_object.set_euler_angles(sidewalk, 0, 0, 0)
-	gh_object.set_scale(sidewalk, 1, 1, 1)
-
-	gh_object.compute_faces_normal(sidewalk)
-	gh_object.compute_vertices_normal(sidewalk)
-end  
-if (wall > 0) then
-	gh_model.load_textures(wall, model_directory)
-
-	wall_norm_tex = gh_texture.create_from_file_v2(model_directory .. "BrickSmallBrown0257_1_S_Normal.jpg", PF_U8_RGB, 1, 1)
-	width, height, depth = gh_texture.get_size(wall_norm_tex)
-	gh_utils.trace(width .. "," .. height .. "," .. depth)
-	gh_object.set_materials_texture_unit_offset(wall, 0)
-
-	gh_object.set_position(wall, 5.8, 2.09, 0)
-	gh_object.set_euler_angles(wall, 0, 0, 0)
-	gh_object.set_scale(wall, 1, 1, 1)
-
-	gh_object.compute_faces_normal(wall)
-	gh_object.compute_vertices_normal(wall)
-end  
-if (box > 0) then
-	gh_model.load_textures(box, model_directory)
-
-	box_norm_tex = gh_texture.create_from_file_v2(model_directory .. "WoodPlanks_Normal.jpg", PF_U8_RGB, 1, 1)
-
-	gh_object.set_materials_texture_unit_offset(box, 0)
-
-	gh_object.set_position(box, 4.5, 1.5, -5)
-	gh_object.set_euler_angles(box, 180, 0, 0)
-	gh_object.set_scale(box, 2.3, 2.3, 2.3)
-
-end  
-if (aircondition > 0) then
-	gh_model.load_textures(aircondition, model_directory)
-
-	aircondition_norm_tex = gh_texture.create_from_file_v2(model_directory .. "AIRCONDITIONED_Normal.jpg", PF_U8_RGB, 1, 1)
-
-	gh_object.set_materials_texture_unit_offset(aircondition, 0)
-
-	gh_object.set_position(aircondition, 5.5, 6, 4)
-	gh_object.set_euler_angles(aircondition, 0, 0, 0)
-	gh_object.set_scale(aircondition, 1, 1, 1)
-
-end  
-  
-	
-shader_prog = gh_gpu_program.create_from_file("shader.glsl", 0)
-gh_node.set_name(shader_prog, "shader_prog")
-
-shader_shadowMap = gh_gpu_program.create_from_file("shader_shadowMap.glsl", 0)
-gh_node.set_name(shader_shadowMap, "shader_shadowMap")
-
-gh_renderer.set_vsync(1)
-shadowMap_w = 4096
-shadowMap_h = 4096
-shadowMap = gh_render_target.create(shadowMap_w, shadowMap_h)
-
-lightDegree = math.pi / 2 * 3
-lightSource = { - 10, 10, 0 }
-lightLookAt = { 0, 0, 0 }
-
+gx_camera.set_keyboard_speed(10.0)
+-- Light camera
 camera_light = gx_camera.create_perspective(90, 1, 0, 0, shadowMap_w, shadowMap_h, 0.1, 1000)
 gx_camera.init_orientation(camera_light, lightLookAt[1], lightLookAt[2], lightLookAt[3], 20, 170)
-
-last_time = gh_utils.get_elapsed_time()
-
+-- Ortho camera for 2d overlay rendering
 camera_ortho = gh_camera.create_ortho(- winW / 2, winW / 2, - winH / 2, winH / 2, 1.0, 10.0)
 gh_camera.set_viewport(camera_ortho, 0, 0, winW, winH)
 gh_camera.set_position(camera_ortho, 0, 0, 2)
 
-shadowmap_viewer_prog = gh_gpu_program.create_from_file("shadowmap_viewer.glsl", 0)
-gh_node.set_name(shadowmap_viewer_prog, "shadowmap_viewer_prog") -- useful for the live coding manager. 
-
-fullscreen_quad = gh_mesh.create_quad(800, 600)
+-- +++++++++++++++ Other render options +++++++++++++++
+gh_renderer.set_vsync(1)
+last_time = gh_utils.get_elapsed_time()
--- update.lua
+++ update.lua
@@ -1,61 +1,76 @@
 
+-- Calc elapsed time
 local elapsed_time = gh_utils.get_elapsed_time()
 local dt = elapsed_time - last_time
 last_time = elapsed_time
 
+-- +++++++++++++++ Handle input +++++++++++++++
 local KC_LEFT = 203
 local KC_RIGHT = 205
-local is_down = gh_input.keyboard_is_key_down(KC_RIGHT)
-if (is_down == 1) then
+-- Move light
+if (gh_input.keyboard_is_key_down(KC_RIGHT) == 1) then
 	lightDegree = lightDegree + dt * 0.6
 end
 if (gh_input.keyboard_is_key_down(KC_LEFT) == 1) then
 	lightDegree = lightDegree - dt * 0.6
-	gh_utils.trace("L " .. lightSource[1] .. "," .. lightSource[3])
 end
 lightSource[1] = math.sin(lightDegree) * 10
 lightSource[3] = math.cos(lightDegree) * 10
-
-gh_camera.set_position(camera_light, lightSource[1], lightSource[2], lightSource[3])
-gh_camera.set_lookat(camera_light, lightLookAt[1], lightLookAt[2], lightLookAt[3], 1)
+-- Update main camera
 gx_camera.update(camera, dt)
 
+-- Set light camera from light position
+gh_camera.set_position(camera_light, lightSource[1], lightSource[2], lightSource[3])
+gh_camera.set_lookat(camera_light, lightLookAt[1], lightLookAt[2], lightLookAt[3], 1)
 
+-- +++++++++++++++ Render shadow map +++++++++++++++
+-- Render to a texture
 gh_render_target.bind(shadowMap)
 gh_camera.bind(camera_light)
+gh_gpu_program.bind(shader_shadowMap)
 
+-- Clear
 gh_renderer.clear_color_depth_buffers(0.2, 0.2, 0.2, 1.0, 1.0)
 gh_renderer.set_depth_test_state(1)
 
-gh_gpu_program.bind(shader_shadowMap)
+-- Render models
 gh_object.render(model)
 gh_object.render(street)
 gh_object.render(sidewalk)
 gh_object.render(wall)
 gh_object.render(box)
 gh_object.render(aircondition)
-gh_gpu_program.bind(0)
 
+-- Unbind
+gh_gpu_program.bind(0)
 gh_render_target.unbind(shadowMap)
 
-
+-- +++++++++++++++ Render shadowed scene +++++++++++++++
+-- Clear
 gh_renderer.clear_color_depth_buffers(0.2, 0.2, 0.2, 1.0, 1.0)
 gh_renderer.set_depth_test_state(1)
 
+-- Bindings
 gh_camera.bind(camera)
 gh_gpu_program.bind(shader_prog)
+
+-- Set parameters
+-- Set light direction, position, power and view matrix
 gh_gpu_program.uniform4f(shader_prog, "lightDir", lightLookAt[1] - lightSource[1], lightLookAt[2] - lightSource[2], lightLookAt[3] - lightSource[3], 0)
 gh_gpu_program.uniform4f(shader_prog, "lightPos", lightSource[1], lightSource[2], lightSource[3], 0)
 gh_gpu_program.uniform1f(shader_prog, "lightPower", 1.2)
-gh_gpu_program.uniform4f(shader_prog, "camera", gh_camera.get_view(camera))
 gh_gpu_program.uniform_camera_matrices(shader_prog, camera_light, "lightViewMatrix", "lightProjectionMatrix")
+-- Set camera vector
+gh_gpu_program.uniform4f(shader_prog, "camera", gh_camera.get_view(camera))
+-- Set texture indices
 gh_gpu_program.uniform1i(shader_prog, "tex0", 0)
 gh_gpu_program.uniform1i(shader_prog, "tex1", 1)
 gh_gpu_program.uniform1i(shader_prog, "shadowMap", 2)
+
+-- Bind shadow map
 gh_texture.rt_color_bind(shadowMap, 2)
    
--- Render the triangle and the grid
---
+-- Render models with normal maps
 gh_texture.bind(model_norm_tex, 1)
 gh_object.render(model)
 gh_texture.bind(street_norm_tex, 1)
@@ -69,11 +84,17 @@ gh_object.render(box)
 gh_texture.bind(aircondition_norm_tex, 1)
 gh_object.render(aircondition)
 
-
+-- +++++++++++++++ Render shadow map to screen +++++++++++++++
 gh_renderer.set_depth_test_state(0)
+
+-- Bindings
 gh_gpu_program.bind(shadowmap_viewer_prog)
+gh_camera.bind(camera_ortho)
+
+-- Set shadow map
 gh_gpu_program.uniform1i(shader_prog, "tex0", 0)
 gh_texture.rt_color_bind(shadowMap, 0)
-gh_camera.bind(camera_ortho)
+
+-- Render fullscreen squad into the upper right corner
 gh_object.set_position(fullscreen_quad, winW / 2 - 400 - 10, winH / 2 - 300 - 60, 0)
 gh_object.render(fullscreen_quad)
